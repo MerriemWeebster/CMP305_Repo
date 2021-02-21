755857e2f027823ff6c728bf289f012083bbde60
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2021-02-21 14:33:31 +0400

    Ex 2

--- Lab3/Ex2Source.cpp
+++ Lab3/Ex2Source.cpp
@@ -12,7 +12,7 @@ struct SingleNode
 	SingleNode(const Object & d = Object{}, SingleNode * n = nullptr) //constructor initializing data to empty object and 
 		: data{ d }, next{ n } { } //next pointer to null 
 };
-/*
+
 int main()
 {
 	SingleNode<string>* queueHead = nullptr;
@@ -29,18 +29,48 @@ int main()
 			cin >> customerName;
 			//your code goes here
 			
+			if(queueHead == nullptr || queueTail == nullptr)
+			{
+				queueHead = new SingleNode<string>(customerName);
+				queueTail = queueHead;
+			}
+			else
+			{
+				SingleNode<string>* tempPtr = queueTail;
+				queueTail = new SingleNode<string>(customerName);
+				tempPtr->next = queueTail;
+			}
 		}
-			
-		
 		else if (choice == 2) //delete
 		{
-			cout << "Serving " << queueHead->data << endl;
-			//your code goes here
-
+		    if(queueHead == nullptr)
+		    {
+		        cout << "Queue Empty\n";
+		    }
+		    else
+		    {
+	       		cout << "Serving " << queueHead->data << endl;
+    			//your code goes here
+    			
+    		    SingleNode<string>* tempPtr = queueHead;
+                queueHead = queueHead->next;
+                delete tempPtr;   
+		    }
 		}
 		else if (choice == 3) //print
 		{
 			//your code goes here
+			int count = 0;
+		    SingleNode<string>* indexPtr = queueHead;
+		    
+		    while(indexPtr != nullptr)
+		    {
+		        cout << indexPtr->data << "\t";
+		        indexPtr = indexPtr->next;
+		        count++;
+		    }
+
+            cout << "\nCount = " << count << endl;
 		}
 		
 		cout << "1. Stand in the Queue \n2. Serve a Customer \n3. Print Customer names waiting \n4. Exit \nEnter your choice: ";
@@ -50,5 +80,3 @@ int main()
 	
 	return 0;
 }
-
-*/
