5cb44c12a4eab4681760a4e06fee87a2b8ed02a8
Rohan <rohanmitra8@gmail.com>
2021-03-21 14:05:00 +0400

    Menu made

--- Lab7/Route Planner.cpp
+++ Lab7/Route Planner.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
+
+int menu() {
+	cout << "Menu:" << endl;
+	cout << "1.Load Map" << endl;
+	cout << "2.Display Map" << endl;
+	cout << "3.Set Start" << endl;
+	cout << "4.Set Goal" << endl;
+	cout << "5.Find Path DFS" << endl;
+	cout << "6.Find Path BFS" << endl;
+	cout << "7.Find Path DA" << endl;
+	cout << "8.Quit" << endl;
+	int choice;
+	cout << "Enter your choice: ";
+	cin >> choice;
+	return choice;
+}
+
 int main() {
 
 	return 0;
