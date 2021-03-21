ce0a9005594262db7782268cb3db8c3a1cb3ddc7
Rohan <rohanmitra8@gmail.com>
2021-03-21 14:10:14 +0400

    switch case for menu added

--- Lab7/Route Planner.cpp
+++ Lab7/Route Planner.cpp
@@ -11,7 +11,8 @@ int menu() {
 	cout << "5.Find Path DFS" << endl;
 	cout << "6.Find Path BFS" << endl;
 	cout << "7.Find Path DA" << endl;
-	cout << "8.Quit" << endl;
+	cout << "8.Display Path" << endl;
+	cout << "9.Quit" << endl;
 	int choice;
 	cout << "Enter your choice: ";
 	cin >> choice;
@@ -19,6 +20,33 @@ int menu() {
 }
 
 int main() {
-
+	int choice;
+	do{
+		choice = menu();
+	} while (!(choice >= 1 and choice <= 9));
+	bool quit = false;
+	while (choice >= 1 and choice <= 9 and not quit) {
+		switch (choice) {
+		case 1:
+			loadMap(); break;
+		case 2:
+			displayMap(); break;
+		case 3:
+			setStart(); break;
+		case 4:
+			setGoal(); break;
+		case 5:
+			DFS(); break;
+		case 6:
+			BFS(); break;
+		case 7:
+			DA(); break;
+		case 8:
+			displayPath(); break;
+		case 9:
+			cout << "Good bye!! :) " << endl;
+			quit = true;
+		}
+	}
 	return 0;
 }
