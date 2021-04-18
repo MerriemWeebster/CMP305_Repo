eddbe644c63ecd129f6a3366447ac37e0c83023f
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2021-04-18 15:06:13 +0400

    Changes

--- Lab10/Ex1Source.cpp
+++ Lab10/Ex1Source.cpp
@@ -13,25 +13,35 @@ struct TreeNode
 };
 
 template <typename Object>
+int depth(TreeNode<Object>* root, TreeNode<Object>* node, int dep = 0) {
+    if (root == node) return dep;
+    if (root == nullptr) return 0;
+    int dep1 = 0, dep2 = 0;
+    if (root->nextSibling != nullptr) {
+        dep1 = depth(root->nextSibling, node, dep);
+    }
+    if (root->firstChild != nullptr) {
+        dep2 = depth(root->firstChild, node, dep + 1);
+    }
+    if (dep1 > dep2) return dep1;
+    else return dep2;
+}
+
+
+template <typename Object>
 int height(TreeNode<Object> *node, int d=0)
 {
+  int hSibling = -1, hChild = -1;
+
+  if(node->nextSibling != nullptr)
+    hSibling = height(node->nextSibling, d);
+
   if(node->firstChild != nullptr)
-  {
-    if(node->nextSibling == nullptr)
-      return height(node->firstChild, d + 1);
-    else
-    {
-      int hSibling = height(node->nextSibling, d), hChild = height( node->firstChild, d + 1);
-      return (hSibling > hChild) ? hSibling : hChild;
-    }
-  }
-  else
-  {
-    if(node->nextSibling == nullptr)
-      return d;
-    else
-      return height(node->nextSibling, d);
-  }
+    hChild = height( node->firstChild, d + 1);
+
+  int hMax = (hSibling > hChild) ? hSibling : hChild;
+
+  return (hMax > d) ? hMax : d;
 }
 
 int main()
@@ -50,9 +60,9 @@ int main()
     TreeNode<char> *l = new TreeNode<char>('L', nullptr, nullptr);
     TreeNode<char> *m = new TreeNode<char>('M', nullptr, nullptr);
     TreeNode<char> *n = new TreeNode<char>('N', nullptr, nullptr);
-    TreeNode<char> *o = new TreeNode<char>('N', nullptr, nullptr);
-    TreeNode<char> *p = new TreeNode<char>('N', nullptr, nullptr);
-    TreeNode<char> *q = new TreeNode<char>('N', nullptr, nullptr);
+    TreeNode<char> *o = new TreeNode<char>('O', nullptr, nullptr);
+    TreeNode<char> *p = new TreeNode<char>('P', nullptr, nullptr);
+    TreeNode<char> *q = new TreeNode<char>('Q', nullptr, nullptr);
     
     root->firstChild = b;
     b->nextSibling = c;
@@ -67,9 +77,10 @@ int main()
     n->firstChild = o;
     o->nextSibling = p;
     o->firstChild = q;
-    
-  //Continue creating TreeNodes
 
+    cout << "The height of the tree is: " << height(root) << endl;
+    cout << "The depth of node Q is: " << depth(root, q) << endl;
+    cout << "The depth of node O is: " << depth(root, o) << endl;
+    cout << "The depth of node D is: " << depth(root, d) << endl;
+    cout << "The depth of node A is: " << depth(root, root) << endl;
 }
-
-
